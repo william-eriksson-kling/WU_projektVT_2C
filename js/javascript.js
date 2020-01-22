61a54a5a89caa61edec6bf3f5a75ba5662b654a2
william-eriksson-kling <55193729+william-eriksson-kling@users.noreply.github.com>
2020-01-22 14:52:27 +0100

    1.
    Css, "skelett", meny

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function toggleMeny() {
+
+    document.querySelector(".navLinks").classList.toggle("navActive");
+    document.querySelector(".burger").classList.toggle("toggle");
+
+}
+
+document.querySelector(".burger").addEventListener("click", toggleMeny);
